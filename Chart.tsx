230472ce85c49d576650f8523b326709316f07c6
cafesanu <cafesanu@gmail.com>
2020-09-03 17:30:18 -0400

    Initial commit

---
+++ Chart.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Chart = () => {
+  return (
+    <h1>Hello {name}!</h1>
+  );
+};
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-vis
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-vis)
