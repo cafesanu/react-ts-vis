e2afe2385ebe0cb6e554a09d622142c4ffc50335
cafesanu <cafesanu@gmail.com>
2020-09-03 17:34:51 -0400

    2 files modified

--- Chart.tsx
+++ Chart.tsx
@@ -1,7 +1,36 @@
 import React from 'react';
+import {
+  XYPlot,
+  XAxis,
+  YAxis,
+  VerticalGridLines,
+  HorizontalGridLines,
+  AreaSeries
+} from 'react-vis';
+import { random } from 'faker';
+
+const random0to60 = () => random.number({ min: 0, max: 60 });
+const generateSingleData = (num = 12) => {
+  const d = [...Array(num)]
+    .fill(1)
+    .map((_, i) => ({
+      x: i+1,
+      y: random0to60()
+    }))
+  return d;
+};
 
 export const Chart = () => {
   return (
-    <h1>Hello {name}!</h1>
+    <XYPlot width={300} height={300}>
+      <VerticalGridLines />
+      <HorizontalGridLines />
+      <XAxis />
+      <YAxis />
+      <AreaSeries
+        className="area-series-example"
+        data={generateSingleData()}
+      />
+    </XYPlot>
   );
 };
